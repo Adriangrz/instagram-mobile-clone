38cb4a3cb72ff16ba9f7aa25c1a64a4b75831e1a
Adriangrz <adriang1412g@gmail.com>
2022-11-21 19:12:41 +0100

    Update supabaseClient

--- src/utilities/supabaseClient.ts
+++ src/utilities/supabaseClient.ts
@@ -1,15 +1,14 @@
+import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { setupURLPolyfill } from 'react-native-url-polyfill';
 
-setupURLPolyfill();
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
-
-export const supaBaseclient = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    detectSessionInUrl: false,
-    storage: AsyncStorage,
-  },
-});
+export const supaBaseclient = createClient(
+  'https://jvneoinifrjqltrrxesb.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2bmVvaW5pZnJqcWx0cnJ4ZXNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1NTAwOTYsImV4cCI6MTk4MjEyNjA5Nn0.YYIUu3UKyNAxEh5Y5_elQxkV3uWHvu3aOjDS4wmyqvg',
+  {
+    auth: {
+      detectSessionInUrl: false,
+      storage: AsyncStorage,
+    },
+  }
+);
