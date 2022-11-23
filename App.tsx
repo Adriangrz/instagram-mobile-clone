09b47c190bcdad2f1791a57e63174ac48801d4fc
Adriangrz <adriang1412g@gmail.com>
2022-11-23 13:53:44 +0100

    Add query client provider to app file

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { AuthContextProvider } from './src/contexts/AuthContext';
 import { RootNavigator } from './src/screens/RootNavigator';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <AuthContextProvider>
-      <RootNavigator />
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+      </QueryClientProvider>
     </AuthContextProvider>
   );
 }
